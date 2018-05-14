729046331702f448a5d96a2278cc6fc6e68b5372
minkang7 <36276858+minkang7@users.noreply.github.com>
2018-05-14 00:01:34 -0400

    Update sensorCode.ino

--- sensorCode/sensorCode.ino
+++ sensorCode/sensorCode.ino
@@ -1,18 +1,16 @@
 #define SENSORPINA A0 // x axis
 #define SENSORPINB A1 // y axis
-#define SENSORPINRST A2 // rst axis
+
 
 unsigned long targetTime=0;
 const unsigned long interval=100; //TODO: How fast should we read
 int x = 100;
 int y = 100;
-int rstValue;
 int prev_x = 100;
 int prev_y = 100;
 void setup(){
   pinMode(SENSORPINA, INPUT);
   pinMode(SENSORPINB, INPUT);
-  pinMode(SENSORPINRST, INPUT);
   Serial.begin(115200);
 }
 
@@ -23,7 +21,6 @@ void loop(){
     //Add other sensor read outs
     x = analogRead(SENSORPINA);
     y = analogRead(SENSORPINB);
-    rstValue = analogRead(SENSORPINRST);
     if (x != prev_x || y != prev_y) {
       // convert values into a string, combine them into a string that can be understood by server.js
       // send the string over serial
@@ -32,12 +29,8 @@ void loop(){
       prev_x = x;
       prev_y = y;
     }
-    if (rstValue > 10) {
-      Serial.println("rst");
-    }
-  }
-  // TODO: Detect if you want to reset the screen(shake the etch-a-sketch)
 
-  // TODO: write the reset message(see server.js) to the serial port
+  }
+
 
 }
